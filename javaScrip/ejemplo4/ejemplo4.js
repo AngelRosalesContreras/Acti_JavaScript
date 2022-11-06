269717d7595433791208cbe06e99439706eceec4
AngelRosalesContreras <angel.r.c.utt.4@gmail.com>
2022-11-05 21:14:41 -0600

    delete

--- javaScrip/ejemplo4/ejemplo4.js
+++
@@ -1,8 +0,0 @@
-function abrirRegalo() {
-  const image = event.currentTarget;
-  image.src = 'images/giphy.gif';
-  image.removeEventListener('click',abrirRegalo);
-}
-
-const image =   document.querySelector('img');
-image.addEventListener('click',abrirRegalo);
--- javaScrip/ejemplo9/js/foto-list.js
+++
@@ -1,9 +0,0 @@
-const PHOTO_LIST = ['images/conejo.jpg',
-  'images/lobo.jpg',
-  'images/gato.jpg',
-  'images/panda.jpg',
-  'images/perro.jpg',
-  'images/suricata.jpg',
-  'images/tigre.jpg',
-  'images/zorro.jpg'
-];
