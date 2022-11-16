aa1de73aeb3c462664e48946c86bbf1a9be90f27
AngelRosalesContreras <angel.r.c.utt.4@gmail.com>
2022-11-16 11:56:25 -0600

    V1.0

--- javaScrip2/ejemplo13/js/boton.js
+++ javaScrip2/ejemplo13/js/boton.js
@@ -5,10 +5,13 @@ class Boton {
     this.onClick = this.onClick.bind(this);
     const boton = document.createElement('button');
     boton.textContent = texto;
-    boton.addEventListener('click',this.onClick)
+    boton.addEventListener('click',this.onClick);
     this.contenedorElemento.append(boton);
   }
   onClick(){
-    console.log('Hiciste clic en: '+this.texto);
-  }
+    console.log('Hiciste clic en: '+ this.texto);
+    const eventInfo = { nombreBoton: this.texto };
+    document.dispatchEvent(new CustomEvent('boton-click',{detail:
+      eventInfo}));
+    }
 }
